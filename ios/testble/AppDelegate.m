fca95e9637a1b9f187c96cf134bb6926db75ff6d
inkloud@infiswift.tech <inkloud@infiswift.tech>
2022-11-27 14:19:22 +0530

    updated code

--- ios/testble/AppDelegate.m
+++ ios/testble/AppDelegate.m
@@ -3,56 +3,66 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
-#import <React/RCTAppSetupUtils.h>
+#import <GoogleMaps/GoogleMaps.h>
+#import <AppCenterReactNative.h>
+#import <AppCenterReactNativeAnalytics.h>
+#import <AppCenterReactNativeCrashes.h>
+#import <BackgroundTasks/BackgroundTasks.h>
 #include "BLEScan.h"
 
+#import "TESTBLE-Swift.h"
 
-#if RCT_NEW_ARCH_ENABLED
-#import <React/CoreModulesPlugins.h>
-#import <React/RCTCxxBridgeDelegate.h>
-#import <React/RCTFabricSurfaceHostingProxyRootView.h>
-#import <React/RCTSurfacePresenter.h>
-#import <React/RCTSurfacePresenterBridgeAdapter.h>
-#import <ReactCommon/RCTTurboModuleManager.h>
+#ifdef FB_SONARKIT_ENABLED
+#import <FlipperKit/FlipperClient.h>
+#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
+#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
+#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
+#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
+#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 
-#import <react/config/ReactNativeConfig.h>
-
-static NSString *const kRNConcurrentRoot = @"concurrentRoot";
-
-@interface AppDelegate () <RCTCxxBridgeDelegate, RCTTurboModuleManagerDelegate> {
-  RCTTurboModuleManager *_turboModuleManager;
-  RCTSurfacePresenterBridgeAdapter *_bridgeAdapter;
-  std::shared_ptr<const facebook::react::ReactNativeConfig> _reactNativeConfig;
-  facebook::react::ContextContainer::Shared _contextContainer;
+static void InitializeFlipper(UIApplication *application) {
+  FlipperClient *client = [FlipperClient sharedClient];
+  SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
+  [client addPlugin:[[FlipperKitLayoutPlugin alloc] initWithRootNode:application withDescriptorMapper:layoutDescriptorMapper]];
+  [client addPlugin:[[FKUserDefaultsPlugin alloc] initWithSuiteName:nil]];
+  [client addPlugin:[FlipperKitReactPlugin new]];
+  [client addPlugin:[[FlipperKitNetworkPlugin alloc] initWithNetworkAdapter:[SKIOSNetworkAdapter new]]];
+  [client start];
 }
-
-@end
 #endif
+
+static NSString* TaskID = @"tech.infiswift.PulseApp.BLEScan";
 
 @implementation AppDelegate
 
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+  NSLog(@"BLEScan: Background mode");
+  [self taskScheduler];
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  RCTAppSetupPrepareApp(application);
-
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-
-#if RCT_NEW_ARCH_ENABLED
-  _contextContainer = std::make_shared<facebook::react::ContextContainer const>();
-  _reactNativeConfig = std::make_shared<facebook::react::EmptyReactNativeConfig const>();
-  _contextContainer->insert("ReactNativeConfig", _reactNativeConfig);
-  _bridgeAdapter = [[RCTSurfacePresenterBridgeAdapter alloc] initWithBridge:bridge contextContainer:_contextContainer];
-  bridge.surfacePresenter = _bridgeAdapter.surfacePresenter;
+#if DEBUG
+  NSLog(@"Not using AppCenter");
+#else
+  [AppCenterReactNative register];
+  [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];
+  [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
 #endif
 
-  NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"testble", initProps);
+#ifdef FB_SONARKIT_ENABLED
+  InitializeFlipper(application);
+#endif
+
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"PulseApp"
+                                            initialProperties:nil];
 
   if (@available(iOS 13.0, *)) {
-    rootView.backgroundColor = [UIColor systemBackgroundColor];
+      rootView.backgroundColor = [UIColor systemBackgroundColor];
   } else {
-    rootView.backgroundColor = [UIColor whiteColor];
+      rootView.backgroundColor = [UIColor whiteColor];
   }
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
@@ -61,80 +71,116 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   
-  BLEScan *scanner = [[BLEScan alloc] initModule];
-
+  [BLEScan.sharedManager startDeviceScan:nil options:nil];
+  
+  [self configureTask];
   
   return YES;
 }
 
-/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
-///
-/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
-/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
-/// @return: `true` if the `concurrentRoot` feture is enabled. Otherwise, it returns `false`.
-- (BOOL)concurrentRootEnabled
-{
-  // Switch this bool to turn on and off the concurrent root
-  return true;
+-(void)configureTask{
+    [[BGTaskScheduler sharedScheduler] registerForTaskWithIdentifier:TaskID
+                                                          usingQueue:nil
+                                                       launchHandler:^(BGTask *task) {
+        [self handleAppRefreshTask:task];
+    }];
 }
 
-- (NSDictionary *)prepareInitialProps
-{
-  NSMutableDictionary *initProps = [NSMutableDictionary new];
+-(void)handleAppRefreshTask:(__kindof BGTask *)task  {
+  NSLog(@"BLEScan: Executing task: %@", TaskID);
+  
+  CBUUID *id2 = [CBUUID UUIDWithString:@"00002080-0000-1000-8000-00805f9b34fb" ];
+  NSArray *ids = @[id2];
+  
+  [BLEScan.sharedManager getConnectedPeripherals:ids];
+  
+  // wait for 15 seconds before marking current task as complete and rescheduling it again
+  [self waitForTaskBeforeReschedule:6.0 task:task];
+}
+- (void)waitForTaskBeforeReschedule: (NSTimeInterval) interval
+                                task: (__kindof BGTask *)task {
+    [NSTimer scheduledTimerWithTimeInterval:interval
+                                     target:self
+                                   selector:@selector(scheduleTaskAfterInterval:)
+                                   userInfo:task
+                                    repeats:NO];
+}
 
-#ifdef RCT_NEW_ARCH_ENABLED
-  initProps[kRNConcurrentRoot] = @([self concurrentRootEnabled]);
-#endif
+- (void)scheduleTaskAfterInterval: (NSTimer *)timer {
+  // Complete previous task
+//  BGTask* task = [timer userInfo];
+//  [task setTaskCompletedWithSuccess:true];
+  
+  // Schedule fresh task
+  [self taskScheduler];
+}
 
-  return initProps;
+- (void)taskScheduler {
+  NSLog(@"BLEScan: Scheduling background task: %@", TaskID);
+  
+  BGProcessingTaskRequest *request = [[BGProcessingTaskRequest alloc] initWithIdentifier:TaskID];
+  request.earliestBeginDate = [NSDate dateWithTimeIntervalSinceNow:2*60];
+  NSError *error = NULL;
+  
+  BOOL success = [[BGTaskScheduler sharedScheduler] submitTaskRequest:request error:&error];
+  if (!success) {
+      NSLog(@"BLEScan: Failed to submit request: %@",error);
+  }
+  
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
 #if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
-#if RCT_NEW_ARCH_ENABLED
 
-#pragma mark - RCTCxxBridgeDelegate
+#pragma mark - Core Data stack
 
-- (std::unique_ptr<facebook::react::JSExecutorFactory>)jsExecutorFactoryForBridge:(RCTBridge *)bridge
-{
-  _turboModuleManager = [[RCTTurboModuleManager alloc] initWithBridge:bridge
-                                                             delegate:self
-                                                            jsInvoker:bridge.jsCallInvoker];
-  return RCTAppSetupDefaultJsExecutorFactory(bridge, _turboModuleManager);
+@synthesize persistentContainer = _persistentContainer;
+
+- (NSPersistentContainer *)persistentContainer {
+    // The persistent container for the application. This implementation creates and returns a container, having loaded the store for the application to it.
+    @synchronized (self) {
+        if (_persistentContainer == nil) {
+            _persistentContainer = [[NSPersistentContainer alloc] initWithName:@"CoreDataModel"];
+            [_persistentContainer loadPersistentStoresWithCompletionHandler:^(NSPersistentStoreDescription *storeDescription, NSError *error) {
+                if (error != nil) {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    
+                    /*
+                     Typical reasons for an error here include:
+                     * The parent directory does not exist, cannot be created, or disallows writing.
+                     * The persistent store is not accessible, due to permissions or data protection when the device is locked.
+                     * The device is out of space.
+                     * The store could not be migrated to the current model version.
+                     Check the error message to determine what the actual problem was.
+                     */
+                    NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+                    abort();
+                }
+            }];
+        }
+    }
+    return _persistentContainer;
 }
 
-#pragma mark RCTTurboModuleManagerDelegate
+#pragma mark - Core Data Saving support
 
-- (Class)getModuleClassFromName:(const char *)name
-{
-  return RCTCoreModulesClassProvider(name);
+- (void)saveContext {
+    NSManagedObjectContext *context = self.persistentContainer.viewContext;
+    NSError *error = nil;
+    if ([context hasChanges] && ![context save:&error]) {
+        // Replace this implementation with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        NSLog(@"Unresolved error %@, %@", error, error.userInfo);
+        abort();
+    }
 }
-
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
-                                                      jsInvoker:(std::shared_ptr<facebook::react::CallInvoker>)jsInvoker
-{
-  return nullptr;
-}
-
-- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const std::string &)name
-                                                     initParams:
-                                                         (const facebook::react::ObjCTurboModule::InitParams &)params
-{
-  return nullptr;
-}
-
-- (id<RCTTurboModule>)getModuleInstanceFromClass:(Class)moduleClass
-{
-  return RCTAppSetupDefaultModuleFromClass(moduleClass);
-}
-
-#endif
 
 @end
--- ios/testble/BLEScan.m
+++ ios/testble/BLEScan.m
@@ -7,90 +7,242 @@
 
 #import <Foundation/Foundation.h>
 #import "BLEScan.h"
+#import "AppDelegate.h"
+#import "TESTBLE-Swift.h"
 
-@interface BLEScan() <CBCentralManagerDelegate>
+@class NSString;
 
-@property (strong, nonatomic) CBCentralManager *manager;
-@property CBPeripheral *dot;
+@interface BLEScan() <CBCentralManagerDelegate, CLLocationManagerDelegate> {
+  AppDelegate *appDelegate;
+  NSManagedObjectContext *context;
+  NSArray *dictionaries;
+}
 
+@property (strong, nonatomic) CBCentralManager * _Nonnull centralManager;
+@property (strong, nonatomic) CLLocationManager * _Nonnull locationManager;
+@property (strong) NSMutableDictionary<NSString*, CBPeripheral* > *  _Nullable discoveredPeripherals;
 
 @end
 
+
+static NSString* RestoreIdentifier = @"PulseAppBLEIdentifier";
 @implementation BLEScan
 
-- (instancetype) initModule {
+@synthesize centralManager = _centralManager;
+@synthesize locationManager = _locationManager;
+@synthesize discoveredPeripherals = _discoveredPeripherals;
+
+#pragma mark Singleton Methods
+
++ (id) sharedManager {
+  NSLog(@"BLEScan: sharedManager called");
+  static BLEScan *sharedMyManager = nil;
+  static dispatch_once_t onceToken;
+  
+  dispatch_once(&onceToken, ^{
+    sharedMyManager = [[self alloc] init];
+  });
+  
+  return sharedMyManager;
+}
+
+
+#pragma mark initializers
+
+- (id) init {
   NSLog(@"BLEScan: init");
+  
   if (self = [super init]) {
-    _manager = [[CBCentralManager alloc] initWithDelegate:self queue:dispatch_get_main_queue() options:nil];
-    NSLog(@"BLEScan: state %li", (long)_manager.state);
+    // init CoreBluetooth central manager
+    [self initCBCentralManager];
+    
+    // initialize location manager
+    [self initLocationManager];
+    
+    _discoveredPeripherals = [[NSMutableDictionary alloc] init];
+    
+    appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
+    context = appDelegate.persistentContainer.viewContext;
   }
+  
   return self;
 }
 
-- (void)centralManagerDidUpdateState:(CBCentralManager *)central {
-  // You should test all scenarios
-  NSLog(@"BLEScan: state %li", (long)central.state);
-  if (central.state != CBManagerStatePoweredOn) {
-      return;
-  }
-   
-  if (central.state == CBManagerStatePoweredOn) {
-      // Scan for devices
-    [_manager scanForPeripheralsWithServices:nil options:@{ CBCentralManagerScanOptionAllowDuplicatesKey : @YES }];
-    NSLog(@"Scanning started");
+- (void) initCBCentralManager {
+  _centralManager = [[CBCentralManager alloc] initWithDelegate:self
+                                                         queue:dispatch_get_main_queue()
+                                                       options:@{ CBCentralManagerOptionRestoreIdentifierKey:RestoreIdentifier }];
+}
+
+- (void) initLocationManager {
+  _locationManager = [CLLocationManager alloc];
+  _locationManager.delegate = self;
+  
+//  [_locationManager requestAlwaysAuthorization];
+//  [_locationManager startUpdatingLocation];
+}
+
+
+
+#pragma mark CBCentralManagerDelegate
+
+- (void) centralManagerDidUpdateState:(CBCentralManager *)central {
+  NSLog(@"BLEScan: centralManagerDidUpdateState ");
+  [self beginSearch];
+}
+
+- (void) beginSearch  {
+  switch ([_centralManager state]) {
+
+    case CBManagerStateUnknown:
+      NSLog(@"BLEScan: Unknown state");
+      break;
+    case CBManagerStateResetting:
+      NSLog(@"BLEScan: Resetting");
+      break;
+    case CBManagerStateUnsupported:
+      NSLog(@"BLEScan: BLE unsupported");
+      break;
+    case CBManagerStateUnauthorized:
+      NSLog(@"BLEScan: App is not authorized to use BLE");
+      break;
+    case CBManagerStatePoweredOff:
+      NSLog(@"BLEScan: BLE is powered off");
+      break;
+    case CBManagerStatePoweredOn:
+      NSLog(@"BLEScan: Powered on");
+      CBUUID *id2 = [CBUUID UUIDWithString:@"00002080-0000-1000-8000-00805f9b34fb" ];
+      NSArray *ids = @[ id2];
+      
+      [self startDeviceScan:ids options:nil];
+      [self stopDeviceScanAfter:5.0];
+      break;
   }
 }
 
-- (void)centralManager:(CBCentralManager *)central didDiscoverPeripheral:(CBPeripheral *)peripheral advertisementData:(NSDictionary *)advertisementData RSSI:(NSNumber *)RSSI {
-     
-    NSLog(@"Discovered %@ at %@", peripheral.name, RSSI);
+- (void) centralManager:(CBCentralManager *)central
+  didDiscoverPeripheral:(CBPeripheral *)peripheral
+      advertisementData:(NSDictionary<NSString *,id> *)advertisementData
+                   RSSI:(NSNumber *)RSSI {
+  NSLog(@"BLEScan: Peripheral discovered");
+  
+  [_discoveredPeripherals setObject:peripheral forKey:peripheral.name];
+//  _discoveredPeripherals[peripheral.name] = peripheral;
+  NSString* uuid = [self getServiceUUIDFromAdvertisementData:advertisementData];
+  NSLog(@"BLEScan: Name = %@, UUID = %@", peripheral.name, uuid);
+  
+  // Save discoverd device to CoreData
+//  NSManagedObject *entityObj = [NSEntityDescription insertNewObjectForEntityForName:@"Beacons" inManagedObjectContext:context];
+//  [entityObj setValue:peripheral.identifier forKey:@"identifier"];
+//  [entityObj setValue:peripheral.name forKey:@"name"];
+//  [entityObj setValue:uuid forKey:@"serviceUUID"];
+  
+//  [appDelegate saveContext];
+}
+
+- (void)centralManager:(CBCentralManager *)central didConnectPeripheral:(CBPeripheral*)peripheral {
+  
+  NSLog(@"BLEScan: Peripheral connected %@", peripheral.name);
+}
+
+- (void)centralManager:(CBCentralManager *)central didFailToConnectPeripheral:(CBPeripheral *)peripheral error:(NSError *)error {
+  NSLog(@"BLEScan: failed to connect with error: %@", error);
+}
+
+- (void)centralManager:(CBCentralManager *)central willRestoreState:(NSDictionary<NSString *,id> *)dict {
+  NSLog(@"BLEScan: willRestoreState - %@", dict);
+}
+
+#pragma mark CLLocationManagerDelegate
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations {
+  NSLog(@"BLEScan: %@", locations.lastObject);
+}
+
+
+
+#pragma mark Class methods
+
+/**
+ * Start scanning for BLE devices with ServiceUUIDs
+ */
+- (void) startDeviceScan: (nullable NSArray<CBUUID *> *)serviceUUIDs
+                 options:(nullable NSDictionary<NSString *, id> *)options {
+  if (_centralManager.state == CBManagerStatePoweredOn) {
+    NSLog(@"BLEScan: starting device scan");
+    [_centralManager scanForPeripheralsWithServices:serviceUUIDs options:options];
+    
+    [self stopDeviceScanAfter:5.0];
+  }
+}
+
+/**
+ * Stop scanning of BLE devices after given interval
+ */
+- (void) stopDeviceScanAfter: (NSTimeInterval) interval {
+  [NSTimer scheduledTimerWithTimeInterval:interval
+                                   target:self
+                                 selector:@selector(stopDeviceScan:)
+                                 userInfo:nil
+                                  repeats:NO];
+}
+
+/**
+ * Stop scanning of iBecaons
+ */
+- (void) stopDeviceScan: (NSTimer *)timer {
+  NSLog(@"BLEScan: stopping device scan: %@", _discoveredPeripherals);
+  
+  [_centralManager stopScan];
+  
+  for(NSString *name in _discoveredPeripherals) {
+    CBPeripheral *peripheral = [_discoveredPeripherals objectForKey:name];
+    
+    NSLog(@"BLEScan: PeriName: %@", peripheral);
+    
+    [_centralManager connectPeripheral:peripheral options:nil];
+  }
+  
+//  [self getConnectedPeripherals:<#(NSArray<CBUUID *> * _Nonnull)#>];
+}
+
+/**
+ * Parses advertisementData from iBeacon discovery and returns Service UUID string
+ */
+- (NSString* ) getServiceUUIDFromAdvertisementData: (NSDictionary *)advertisementData {
+  NSString* uuidString;
+  NSDictionary* kCBAdvDataServiceData = advertisementData[@"kCBAdvDataServiceData"];
+  
+  // In iBeacon, we get ServiceUUID as keys and we're interested in the first ServiceUUID
+  NSArray *keys = [kCBAdvDataServiceData allKeys];
+  CBUUID* uuid = (CBUUID *)[keys objectAtIndex: 0];
+  
+  // converting CBUUID advertised by iBeacon to full UUID string based on logic from MultiplayformBLEAdaptor [link: https://github.com/dotintent/MultiPlatformBleAdapter/blob/master/iOS/classes/BleUtils.swift]
+  uuidString = uuid.fullUUIDString;
+  
+  return uuidString;
+}
+
+- (void) getConnectedPeripherals: (NSArray<CBUUID *> *)serviceUUIDs  {
+  NSLog(@"BLEScan: Scanning connected peripherals");
+  
+  NSLog(@"BLEScan: Scanning: %i", [_centralManager isScanning]);
+  
+  NSArray<CBPeripheral *> *peripherals = [_centralManager retrieveConnectedPeripheralsWithServices:serviceUUIDs];
+  
+  for (CBPeripheral* peripheral in peripherals) {
+    NSLog(@"BLEScan: found connected peripheral - %@", peripheral.name);
+  }
+}
+
+- (void) reconnectPeripheral: (CBPeripheral *) peripheral {
+  [_centralManager cancelPeripheralConnection:peripheral];
+  [_centralManager connectPeripheral:peripheral options:nil];
+}
+
+- (void) fetchDevicesFromCoreData {
+  NSFetchRequest *requestExamLocation = [NSFetchRequest fetchRequestWithEntityName:@"Beacons"];
+  NSArray *results = [context executeFetchRequest:requestExamLocation error:nil];
 }
 
 @end
-
-
-//#import "BLEScan.h"
-//@import MultiplatformBleAdapter;
-//
-//@interface BLEScan () <BleClientManagerDelegate>
-//@property(nonatomic) BleClientManager* manager;
-//@property dispatch_queue_t methodQueue;
-//@end
-//
-//@implementation BLEScan
-//{
-//    bool hasListeners;
-//}
-//
-//@synthesize methodQueue = _methodQueue;
-//
-//- (void)dispatchEvent:(NSString * _Nonnull)name value:(id _Nonnull)value {
-////  if (hasListeners) {
-////          [self sendEventWithName:name body:value];
-////  }
-//  NSLog(@"event: %@ :: %@", name, value);
-//
-//  if([name  isEqual: @"StateChangeEvent"] && [value  isEqual: @"PoweredOn"]) {
-//    NSDictionary* options = @{ @"allowDuplicates": @true };
-//    [self startDeviceScan:nil options:options];
-//  }
-//}
-//
-//- (void)createClient: (NSString *) restoreIdentifierKey{
-//  _manager = [BleAdapterFactory getNewAdapterWithQueue:self.methodQueue
-//                                    restoreIdentifierKey:restoreIdentifierKey];
-//  _manager.delegate = self;
-//}
-//
-//
-//- (void) startDeviceScan:(NSArray*)filteredUUIDs
-//                 options:(NSDictionary*)options {
-//  [_manager startDeviceScan:filteredUUIDs options:options];
-//}
-//
-//- (void) stopDeviceScan {
-//  [_manager stopDeviceScan];
-//}
-//
-//@end
-//
